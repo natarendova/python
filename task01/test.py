2a9a7c69b1195ce32dcfa0dc8dd8a0a1b4022046
Nata Rendova <nata.rendova@gmail.com>
2021-10-12 14:57:45 +0300

    Решила)

--- task01/test.py
+++ task01/test.py
@@ -8,9 +8,8 @@ except ValueError:
 if  int(n1) < 0  or int(n1) >100:
      n1 = input ("Введите первое число от 1 до 100: ")
 else:    
-     n1 = n1
-          
-     
+     n1 = int(n1) 
+           
 n2 = input("Введите второе число от 1 до 100: ")
 try:
      n2 = int(n2)
@@ -21,17 +20,13 @@ except ValueError:
 if  int(n2) < 0  or int(n2) >100:
      n2 = input ("Введите первое число от 1 до 100: ")
 else:    
-     n2 = n2
-     
+     n2 = int(n2)      
 
-if n1 > n2:
-     # rez = (n1 / n2)
-      print ( "Результат: " ,round( n1 / n2,3))
-elif n1 == n2:
-      print ( "Результат: " ,round( n1 / n2,3))
-else:
-      #rez = (n2 / n1) 
-      print ( "Результат: ",round( n2 / n1,3))  
+try:
+    rez = round( n1 / n2,3)
+    print ( "Результат: " ,rez)
+except ZeroDivisionError:
+    print ( "Ошибка ")
 
 
 
