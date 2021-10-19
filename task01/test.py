a2728a46600c4a0772f0039370865acbd0ff0b1e
Nata Rendova <nata.rendova@gmail.com>
2021-10-19 09:16:15 +0300

    задача 2

--- task01/test.py
+++ task01/test.py
@@ -1,32 +1,30 @@
-n1 = input("Введите первое число от 1 до 100: ")
-try:
-     n1 = int(n1)
-except ValueError:
-     n1 = input ("Введите первое число от 1 до 100: ")
-     
-
-if  int(n1) < 0  or int(n1) >100:
-     n1 = input ("Введите первое число от 1 до 100: ")
-else:    
-     n1 = int(n1) 
-           
-n2 = input("Введите второе число от 1 до 100: ")
-try:
-     n2 = int(n2)
-except ValueError:
-     n2 = input ("Введите второе число от 1 до 100: ")
-     
-
-if  int(n2) < 0  or int(n2) >100:
-     n2 = input ("Введите первое число от 1 до 100: ")
-else:    
-     n2 = int(n2)      
-
+n1 = -1
+while int(n1) < 0  or int(n1) >100 :
+     try:     
+         n1 = int(input ("Введите первое число от 1 до 100: "))
+     except ValueError:
+         print ( "Ошибка ")
+         
+n2 = -1           
+while int(n2) < 0  or int(n2) >100 :
+     try:  
+         n2 = int(input ("Введите второе число от 1 до 100: "))
+     except ValueError:
+         print ( "Ошибка ")
+         
 try:
     rez = round( n1 / n2,3)
     print ( "Результат: " ,rez)
 except ZeroDivisionError:
     print ( "Ошибка ")
+ 
+
+
+   
+   
+     
+
+
 
 
 
