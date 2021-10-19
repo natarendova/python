4797e6e58500f09feefdbbc96eb7188184762f37
Nata Rendova <nata.rendova@gmail.com>
2021-10-19 12:49:29 +0300

    решение второй задачи+ функция

--- task01/test.py
+++ task01/test.py
@@ -1,16 +1,14 @@
 n1 = -1
-while int(n1) < 0  or int(n1) >100 :
-     try:     
-         n1 = int(input ("Введите первое число от 1 до 100: "))
-     except ValueError:
-         print ( "Ошибка ")
-         
-n2 = -1           
-while int(n2) < 0  or int(n2) >100 :
-     try:  
-         n2 = int(input ("Введите второе число от 1 до 100: "))
-     except ValueError:
-         print ( "Ошибка ")
+n2 = -1
+def fun():
+    while int(n1) < 0  or int(n1) >100  or int(n2) < 0  or int(n2) >100:
+         try:  
+             return  int(input ("Введите  число от 1 до 100: "))
+         except ValueError:
+              print ( "Ошибка ") 
+    
+n1 = fun()  
+n2 = fun()     
          
 try:
     rez = round( n1 / n2,3)
