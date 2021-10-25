45057a637aae7903940f45927b810ab4c85e3d40
Nata Rendova <nata.rendova@gmail.com>
2021-10-25 09:40:38 +0300

    задача 2 доделала

--- task02/tast2.py
+++ task02/tast2.py
@@ -3,6 +3,7 @@ n1 = -1
 print ( "Игра угадай число.")
 mx = int(input('Максимальное: '))
 r = random.randint(1,mx)
+i = 1
 
 def fun():
     while int(n1) < 0  or int(n1) > mx:
@@ -10,15 +11,19 @@ def fun():
              return  int(input ("Введите число  от 0 до {}  " .format(mx)))
          except ValueError:
               print ( "Ошибка ")
-
-while r != n1:
-      n1 = -1
-      n1 = fun()
-      if n1 > r:
-         print ( "Ваше число больше")          
-      elif n1 < r:
-        print ( "Ваше число меньше")    
-      else:
-          print ( "Ура!!!")
-          continue
-
+while i < 6: 
+      while r != n1:
+            n1 = -1      
+            n1 = fun()         
+      
+            if n1 > r:
+               print ( "Ваше число больше")
+               i += 1
+            elif n1 < r:
+                 print ( "Ваше число меньше")
+                 i += 1
+            else:
+                 print ( "Ура!!!Вы угодали c {}-й попытки".format(i))
+            break
+if i == 6:
+   print ( "Попытки закончились!!!")
