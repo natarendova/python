f644aabece0fcedb17aabfaa7ba6dbdec81a803e
Nata Rendova <nata.rendova@gmail.com>
2021-10-26 11:38:01 +0300

    shuffle способ

--- task03/tast3.py
+++ task03/tast3.py
@@ -1,15 +1,17 @@
 import random
 st2 = ''
 i = 0
-r = random.randint(1,10)
+r = '123456789'
 list1 ='qwertyuiopasdfghjklzxcvbnm'
 list3 = list1.upper()
 list2 = '+-/*!&$#?=@<>_'
 st = list1 + list2 + list3 + str(r)
 
-for i in range(10):
-    #r1 = random.randint(1,50)
-    st2 = st2 + (random.choice(st))
-    #st2 = st2 + (st[r1])
+for i in range(10):   
+    st2 = st2 + (random.choice(st))    
+ 
+st3 = list(st2 + random.choice(r) + random.choice(list1) + random.choice(list2) + random.choice(list3))
+random.shuffle( st3)
+s = ''.join(st3)
 
-print ("Пароль: ", st2 + str(r) + random.choice(list1) + random.choice(list2) + random.choice(list3) )
+print ("Пароль: ", s )
