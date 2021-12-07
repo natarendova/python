e56edd9fd337ee5e5f850a72fc34f12a2cfa171e
Nata Rendova <nata.rendova@gmail.com>
2021-12-07 15:53:06 +0300

    цикл for без while

--- task07/tast7.py
+++ task07/tast7.py
@@ -1,24 +1,21 @@
 import random
 byk = 0
 cor = 0
+r = '0123456789'
+print('  Добро пожаловать в игру "Были и Коровы"! ') 
+num = (random.choice(r)) + (random.choice(r))+ (random.choice(r))+(random.choice(r))
 
-
-print('  Добро пожаловать в игру "Были и Коровы"! ')
-num = random.randint(1000,9999)
-num = str(num)
 print(num)
-while True:
-     igr1 = (input('Введите комбинацию 4-х цифр: '))
-     if not 999 < int(igr1) < 10000 :
-           print('Нужны цифры от 1000 to 9999')
-     else:
-         print("Нhhhh")
-         for i in set(num) and set(igr1):
-            
-            if num.find(i) == igr1.find(i):
-               byk += 1
-            else:
-                cor += 1
+
+igr1 = str(input('Введите комбинацию 4-х цифр: '))
+if not 999 < int(igr1) < 10000 :
+   print('Нужны цифры от 1000 to 9999')
+else:
+   for i in range(4):            
+       if num[i] == igr1[i]:
+          byk += 1
+       elif igr1[i] in num: 
+          cor += 1
 print ('byk', byk, 'cor', cor)                      
                      
                
